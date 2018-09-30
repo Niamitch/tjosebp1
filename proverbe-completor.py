a256df564be310e91877216e0d4b77ffae550ac4
Your Name <you@example.com>
2018-09-30 14:34:24 -0400

    added completor class + main structure

--- proverbe-completor.py
+++ proverbe-completor.py
@@ -1,8 +1,23 @@
 import sys
 
+class ProverbeCompletor:
+
+    grammes = {}
+
+    def __init__(self, corpus, n_gramme=1):
+        self.__create_model(corpus, n_gramme)
+
+    def __create_model(self, corpus, n_gramme):
+        pass
+
+    def complete(self, incomplete_proverbe, candidate_words):
+        pass
 
 def main(argv):
-    pass
+    corpus = open('./resources/proverbes.txt')
+    proverbe_completor = ProverbeCompletor(corpus, 2)
+    returned_proverbe = proverbe_completor.complete("a beau mentir qui *** de loin", ["vient", "part", "mange", "programme"])
+    print(returned_proverbe)
 
 
 
